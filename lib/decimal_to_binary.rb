ae92c15cd47e509ee4522daa437e2e5045cb984a
Marj E <marj@marjorieetienne.com>
2020-09-12 05:45:00 -0700

    changed method name and removed copied code

--- lib/decimal_to_binary.rb
+++ lib/decimal_to_binary.rb
@@ -4,31 +4,6 @@
 # The least significant bit is at index 7.
 # Calculate  and return the decimal value for this binary number using
 # the algorithm you devised in class.
-def binary_to_decimal(binary_array)
-  result = 0
-  binary_array.length.times do |index|
-    case index
-    when 0
-      value = binary_array[index] * 2**7
-    when 1
-      value = binary_array[index] * 2**6
-    when 2
-      value = binary_array[index] * 2**5
-    when 3
-      value = binary_array[index] * 2**4
-    when 4
-      value = binary_array[index] * 2**3
-    when 5
-      value = binary_array[index] * 2**2
-    when 6
-      value = binary_array[index] * 2**1
-    when 7
-      value = binary_array[index] * 2**0
-    else
-      #  do something
-      raise NotImplementedError
-    end
-    result += value
-  end
-  return result
+def decimal_to_binary(decimal)
+  raise NotImplementedError
 end
