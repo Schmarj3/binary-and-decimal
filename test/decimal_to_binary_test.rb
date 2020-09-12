cc9084ede37e9611bdcf05ef354b43ce579eadfe
Marj E <marj@marjorieetienne.com>
2020-09-12 05:38:19 -0700

    changed copied binary_to_decimal test req to decimal_to_binary req except last test

--- test/decimal_to_binary_test.rb
+++ test/decimal_to_binary_test.rb
@@ -1,35 +1,37 @@
 require 'minitest/autorun'
 require 'minitest/reporters'
-require_relative '../lib/binary_to_decimal'
+require_relative '../lib/decimal_to_binary'
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
-describe "binary to decimal" do
-  it "From 10011001 to 153" do
-    binary_array = [1, 0, 0, 1, 1, 0, 0, 1]
-    expected_decimal = 153
+describe "decimal to binary" do
+  it "From 153 to 10011001" do
+    decimal = 153
+    expected_binary_array = [1, 0, 0, 1, 1, 0, 0, 1]
 
-    expect(binary_to_decimal(binary_array)).must_equal expected_decimal
+    expect(decimal_to_binary(decimal)).must_equal expected_binary_array
   end
 
-  it "From 00001101 to 13" do
-    binary_array = [0, 0, 0, 0, 1, 1, 0, 1]
-    expected_decimal = 13
+  it "From 13 to 00001101" do
+    decimal = 13
+    expected_binary_array = [0, 0, 0, 0, 1, 1, 0, 1]
 
-    expect(binary_to_decimal(binary_array)).must_equal expected_decimal
+    expect(decimal_to_binary(decimal)).must_equal expected_binary_array
   end
 
-  it "From 10000000 to 128" do
-    binary_array = [1, 0, 0, 0, 0, 0, 0, 0]
-    expected_decimal = 128
+  it "From 128 to 10000000" do
+    decimal = 128
+    expected_binary_array = [1, 0, 0, 0, 0, 0, 0, 0]
 
-    expect(binary_to_decimal(binary_array)).must_equal expected_decimal
+    expect(decimal_to_binary(decimal)).must_equal expected_binary_array
   end
 
-  it "From random binary to decimal" do
-    binary_array = Array.new(8) { rand(0..1) }
-    expected_decimal = binary_array.join.to_s.to_i(2)
+  it "From random decimal to binary" do
+    decimal = binary_array.join.to_s.to_i(2)
+    expected_binary_array = Array.new(8) { rand(0..1) }
+    # binary_array = Array.new(8) { rand(0..1) }
+    # expected_decimal = binary_array.join.to_s.to_i(2)
 
-    expect(binary_to_decimal(binary_array)).must_equal expected_decimal
+    expect(decimal_to_binary(decimal)).must_equal expected_binary_array
   end
 end
