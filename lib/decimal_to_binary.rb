8c6235a0dcad1d4a0fc7f264d68847b13d6c1e16
Marj E <marj@marjorieetienne.com>
2020-09-13 08:08:10 -0700

    fixed decimal_to-binary method

--- lib/decimal_to_binary.rb
+++ lib/decimal_to_binary.rb
@@ -5,5 +5,15 @@
 # Calculate  and return the decimal value for this binary number using
 # the algorithm you devised in class.
 def decimal_to_binary(decimal)
-  raise NotImplementedError
+  binary_array = Array.new(8, 0)
+  count = binary_array.length - 1
+
+  until decimal == 0
+    if decimal % 2 == 1
+      binary_array[count] = 1
+    end
+    decimal /= 2
+    count -= 1
+  end
+  return binary_array
 end
